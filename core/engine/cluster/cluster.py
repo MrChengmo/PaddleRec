bf0affdb3d241550d5eda77afeb0bd28e1bb2446
MrChengmo <cmchengmo@163.com>
2020-08-19 16:19:18 +0800

    fix

--- core/engine/cluster/cluster.py
+++ core/engine/cluster/cluster.py
@@ -328,13 +328,5 @@ class CloudPsGpuEnv(PaddleCloudK8sEnv):
             "submit.k8s_trainers", 1)
         self.cluster_env["K8S_CPU_CORES"] = self.backend_env.get(
             "submit.k8s_cpu_cores", 2)
-        self.cluster_env["K8S_PS_NUM"] = self.backend_env.get(
-            "submit.k8s_ps_num", 1)
-        self.cluster_env["K8S_PS_CORES"] = self.backend_env.get(
-            "submit.k8s_ps_cores", 2)
-        self.cluster_env["K8S_TRAINERS"] = self.backend_env.get(
-            "submit.k8s_trainers", 1)
         self.cluster_env["K8S_GPU_CARD"] = self.backend_env.get(
             "submit.k8s_gpu_card", 1)
-        self.cluster_env["K8S_CPU_CORES"] = self.backend_env.get(
-            "submit.k8s_cpu_cores", 1)
